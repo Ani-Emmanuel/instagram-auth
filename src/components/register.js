14b0c26caee2b92e5851b1762e238b48ea3c6f0e
Emmanuel Ani <mailsforanih@gmail.com>
2021-05-03 16:39:18 +0100

    added first and second section of the registration page

---
+++ src/components/register.js
@@ -0,0 +1,45 @@
+const Register = () => {
+	return (
+		<div className='container'>
+			<div className='registration-section'>
+				<h1 className='registration-header'>Instagram</h1>
+				<div className='registration-form-section'>
+					<form id='register' className='registration-form'>
+						<h2 className='registration-subheader'>
+							Sign up to see photos and videos from your friends.
+						</h2>
+						<div className='fb-login-section'>
+							<button className='fb-login'>Log in with Facebook</button>
+						</div>
+						<div className='line-divider'>
+							<div className='first-line'></div>
+							<div className='or-divider'>or</div>
+							<div className='second-line'></div>
+						</div>
+						<div className='user-info-section'>
+							<input placeholder='Mobile Number or Email' />
+							<input placeholder='Full Name' />
+							<input placeholder='Username' />
+							<input placeholder='Password' />
+							<button className='btn-signup'>Sign Up</button>
+						</div>
+					</form>
+					<div className='terms-text-section'>
+						<p className='terms-text'>
+							By signing up, you agree to our
+							<span className='span-text'> Terms</span>,
+							<span className='span-text'> Data Policy </span>
+							and <span className='span-text'>Cookies Policy</span>.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className='login-section'>
+				<p className="account-login">Have an account? Log in</p>
+			</div>
+			<div className='advart-section'></div>
+		</div>
+	);
+};
+
+export default Register;
