5d25f100860bde6939a4d7d53091103b361bd24e
Emmanuel Ani <mailsforanih@gmail.com>
2021-05-09 23:58:38 +0100

    Added routs and refactored app css module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import './App.css';
-import Register from './components/register';
+import { Switch, Route } from 'react-router-dom';
+import NotFound from './components/404/404';
+import Register from './components/registration/register';
 function App() {
 	return (
 		<div className='App'>
-			<Register />
+			<Switch>
+				<Route exact path='/'>
+					<Register />
+				</Route>
+				<Route path='/terms'>
+					<NotFound />
+				</Route>
+				<Route path='/data-policy'>
+					<NotFound />
+				</Route>
+				<Route path='/cookies-policy'>
+					<NotFound />
+				</Route>
+				<Route path='/login'>
+					<NotFound />
+				</Route>
+				<Route path='/about'>
+					<NotFound />
+				</Route>
+				<Route path='/blog'>
+					<NotFound />
+				</Route>
+				<Route path='/jobs'>
+					<NotFound />
+				</Route>
+				<Route path='/help'>
+					<NotFound />
+				</Route>
+				<Route path='/api'>
+					<NotFound />
+				</Route>
+				<Route path='/privacy'>
+					<NotFound />
+				</Route>
+				<Route path='/top-accounts'>
+					<NotFound />
+				</Route>
+				<Route path='/hashtags'>
+					<NotFound />
+				</Route>
+				<Route path='/location'>
+					<NotFound />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
